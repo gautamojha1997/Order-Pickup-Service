12ddc3dae10c19f7ac4e5a05db13b8f432257905
gautamojha1997 <ojha.gautam97@gmail.com>
2021-07-01 23:39:48 -0500

    Comments Added

--- src/main/java/com/example/orderpickup/service/pickup/PickupServiceImpl.java
+++ src/main/java/com/example/orderpickup/service/pickup/PickupServiceImpl.java
@@ -41,16 +41,18 @@ public class PickupServiceImpl implements PickupService {
      **/
     @Override
     public List<PickupDto> getAllSinglePicking() {
+        log.info("Getting All Single Pickup");
         List<Pickup> pickupAllList = pickupRepository.findAll();
+        log.info("Found all pickup List for filtering single pickups : " + pickupAllList);
         List<Pickup> singlePickupList = new ArrayList<>();
         for(Pickup pickup: pickupAllList){
             if(pickup.getOrdersList().size() == 1)
                 singlePickupList.add(pickup);
         }
-
         List<PickupDto> singlePickupDtoList = new ArrayList<>();
         singlePickupList.forEach(pickup -> singlePickupDtoList.add(pickupMapper.mapToDto(pickup)));
 
+        log.info("Returned All single pickups: " + singlePickupDtoList);
         return singlePickupDtoList;
     }
 
@@ -62,13 +64,16 @@ public class PickupServiceImpl implements PickupService {
     @Override
     @Transactional(isolation = REPEATABLE_READ) //handling concurrent request to create single pickings by multiple client
     public PickupDto createSinglePicking(PickupDto pickupDto) {
+        log.info("Creating Single Pickups");
         if(pickupDto.getOrdersList().size()>1)
             throw new SingleOrderRequiredException("Single Picking requires only single order!");
         Pickup pickup = pickupMapper.mapToEntity(pickupDto);
 
         Pickup newPickup = pickupRepository.save(pickup);
+        log.info("Single Pickup Saved to Repository");
 
         PickupDto pickupDtoResponse = pickupMapper.mapToDto(newPickup);
+        log.info("Single Pickup Returned : " + pickupDtoResponse);
         return pickupDtoResponse;
     }
 
@@ -79,10 +84,12 @@ public class PickupServiceImpl implements PickupService {
      */
     @Override
     public PickupDto cancelSinglePicking(String id) {
+        log.info("Canceling Single Pickup");
         Optional<Pickup> pickup = pickupRepository.findById(id);
         if(!pickup.isPresent())
-            throw new PickupNotFoundException("This Single Picking with" + id +" Not found or Doesn't exist");
+            throw new PickupNotFoundException("This Single Picking with id : " + id +" Not found or Doesn't exist");
         pickup.get().setStatus(Status.CANCELED);
+        log.info("Canceled Single Pickup  : " + pickup);
         return pickupMapper.mapToDto(pickupRepository.save(pickup.get()));
     }
 
@@ -95,7 +102,9 @@ public class PickupServiceImpl implements PickupService {
      **/
     @Override
     public List<PickupDto> getAllBatchPicking() {
+        log.info("Getting All Batch Pickup");
         List<Pickup> pickupAllList = pickupRepository.findAll();
+        log.info("Found all pickup List for filtering batch pickups : " + pickupAllList);
         if(pickupAllList.isEmpty())
             throw new PickupNotFoundException("Pickings Are Empty");
         List<Pickup> batchPickupList = new ArrayList<>();
@@ -108,6 +117,7 @@ public class PickupServiceImpl implements PickupService {
         batchPickupList.forEach(pickup -> {
             batchPickupDtoList.add(pickupMapper.mapToDto(pickup));
         });
+        log.info("Returned All single pickups: " + batchPickupDtoList);
         return batchPickupDtoList;
     }
 
@@ -119,13 +129,16 @@ public class PickupServiceImpl implements PickupService {
     @Override
     @Transactional(isolation = REPEATABLE_READ) //handling concurrent request to create batch pickings by multiple client
     public PickupDto createBatchPicking(PickupDto pickupDto) {
+        log.info("Creating Batch Pickups");
         if(pickupDto.getOrdersList().size()==1)
             throw new BatchOrderRequiredException("For Batch Picking you need multiple orders!!");
         Pickup pickup = pickupMapper.mapToEntity(pickupDto);
 
         Pickup newPickup = pickupRepository.save(pickup);
+        log.info("Batch Pickup Saved to Repository");
 
         PickupDto pickupDtoResponse = pickupMapper.mapToDto(newPickup);
+        log.info("Batch Pickup Returned : " + pickupDtoResponse);
         return pickupDtoResponse;
     }
 
@@ -136,18 +149,31 @@ public class PickupServiceImpl implements PickupService {
      */
     @Override
     public PickupDto cancelBatchPicking(String id) {
+        log.info("Canceling Batch Pickup");
         Optional<Pickup> pickup = pickupRepository.findById(id);
         if(!pickup.isPresent())
-            throw new PickupNotFoundException("This Batch Picking with" + id +" Not found or Doesn't exist");
+            throw new PickupNotFoundException("This Batch Picking with id : " + id +" Not found or Doesn't exist");
         pickup.get().setStatus(Status.CANCELED);
+        log.info("Canceled Batch Pickup  : " + pickup);
         return pickupMapper.mapToDto(pickupRepository.save(pickup.get()));
     }
 
+
+    /***
+     * This method provides the ability to switch from single to batch picking by doing following thing:
+     * 1. Retrieves all single pickup list.
+     * 2. Computes a map which stores list of single pickups based on location as key and if the pickups are not completed or canceled.
+     * 3. Finally it returns batch pickups for all single pickups based on location and batch size provided.
+     * @param location
+     * @param batchSize
+     * returns list of batch pickups.
+     * */
     @Override
     public List<PickupDto> singleToBatch(String location, int batchSize) {
         //Get All Single PickUps List
-        log.info("Entered Service single to batch with location = " + location + "and batchSize = " + batchSize);
+        log.info("Convert all single pickups to batch pickups with location = " + location + "and batchSize = " + batchSize);
         List<Pickup> pickupAllList = pickupRepository.findAll();
+        log.info("Found all pickup List for filtering single pickups : " + pickupAllList);
         if(pickupAllList.isEmpty())
             throw new PickupNotFoundException("Pickings Are Empty");
         List<Pickup> singlePickupList = new ArrayList<>();
@@ -223,7 +249,7 @@ public class PickupServiceImpl implements PickupService {
 
         List<PickupDto> batchPickupDtoList = new ArrayList<>();
         batchedPickups.forEach(p -> batchPickupDtoList.add(pickupMapper.mapToDto(p)));
-        log.info(String.valueOf(batchPickupDtoList));
+        log.info("Converted Single To batch pickup list : " + batchPickupDtoList);
         return batchPickupDtoList;
     }
 
@@ -231,6 +257,7 @@ public class PickupServiceImpl implements PickupService {
     @Override
     public List<PickupDto> batchToSingle(){
         List<Pickup> pickupAllList = pickupRepository.findAll();
+        log.info("Convert all batch pickups to single pickups");
         if(pickupAllList.isEmpty())
             throw new PickupNotFoundException("Pickings Are Empty");
         List<Pickup> batchPickupList = new ArrayList<>();
@@ -240,30 +267,30 @@ public class PickupServiceImpl implements PickupService {
         }
 
         List<Pickup> singlePickups = new ArrayList<>();
-        //log.info(String.valueOf(batchPickupList));
+        log.info("List of All Batch Pickups: " + batchPickupList);
 
         for(Pickup p : batchPickupList){
             Pickup pickup;
             if(p.getStatus()!=Status.COMPLETE||p.getStatus()!=Status.CANCELED){
-
                 //pickup = p;
                 List<Orders> l = p.getOrdersList();
                 for(int i = 0; i < l.size(); i++){
                     pickup = new Pickup();
                     pickup.setOrdersList(Collections.singletonList(l.get(i)));
                     pickup.setStatus(Status.PENDING);
+                    log.info("Converted single pickup: " + pickup + " for batch pickup :" + p.getPickId());
                     pickupRepository.save(pickup);
                     singlePickups.add(pickup);
                 }
+                log.info("Batch Pickup :" + p.getPickId() + " canceled");
                 p.setStatus(Status.CANCELED);
             }
         }
 
         List<PickupDto> singlePickupDtoList = new ArrayList<>();
         singlePickups.forEach(pickup -> singlePickupDtoList.add(pickupMapper.mapToDto(pickup)));
-
+        log.info("Converted Batch To Single pickup list : " + singlePickupDtoList);
         return singlePickupDtoList;
-
     }
 
 }
